98227360971b9d238ef1bd83d019a775a74c9831
Nicholas Gideo <ngideo@gmail.com>
2025-05-09 18:50:08 -0500

    use chat

--- src/MarketAI.Worker/BackgroundAgent.cs
+++ src/MarketAI.Worker/BackgroundAgent.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 namespace MarketAI;
 
@@ -12,44 +13,50 @@ public class BackgroundAgent : BackgroundService
     private readonly ILogger<BackgroundAgent> _logger;
     private readonly DataCacheService _dataService;
     private readonly Kernel _aiKernel;
+    private readonly IChatCompletionService _chat;
 
     public BackgroundAgent(ILogger<BackgroundAgent> logger, 
         DataCacheService dataService ,
-        Kernel ai
+        Kernel ai,
+        IChatCompletionService chat
         )
     {
         ArgumentNullException.ThrowIfNull(dataService);
         ArgumentNullException.ThrowIfNull(ai);
+        ArgumentNullException.ThrowIfNull(chat);
         ArgumentNullException.ThrowIfNull(logger);
         
         _dataService = dataService;
         _aiKernel = ai;
+        _chat = chat;
         _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        //while (!stoppingToken.IsCancellationRequested)
-        //{
-        //    if (_logger.IsEnabled(LogLevel.Information))
-        //    {
-        //        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-        //    }
+        var settings = new OpenAIPromptExecutionSettings
+        {
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+        };
 
-
-
-        //    await Task.Delay(1000, stoppingToken);
-        //}
+        // register a tool
+        //_aiKernel.Plugins.AddFromType<>("name");
 
         var news = await _dataService.GetTodaysNewsFor("MSFT");
 
         // todo: do some AI
 
-        var chatHistory = new ChatHistory();
+        var chatHistory = new ChatHistory("Write a haiku about the stock market");
 
-        var result = await _aiKernel.InvokePromptAsync("Write a haiku about the {topic}", new() { ["topic"] = "stock market" });
+        //var result = await _aiKernel.InvokePromptAsync("Write a haiku about the stock market");
 
-        Console.WriteLine(result.GetValue<string>());
+        var r = await _chat.GetChatMessageContentAsync(chatHistory, kernel: _aiKernel);
+
+        Console.WriteLine(r.ToString());
+
+        chatHistory.AddMessage(r.Role, r.Content ?? "");
+
+        //Console.WriteLine(result.GetValue<string>());
 
         foreach (var s in news)
         {
